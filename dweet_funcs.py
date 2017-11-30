18ec108fd320887162d1dcfb5c4c4b995dbd00d6
KhoomeiK <32777448+KhoomeiK@users.noreply.github.com>
2017-11-29 22:52:50 -0800

    added comments

--- dweet_funcs.py
+++ dweet_funcs.py
@@ -1,8 +1,9 @@
 import json
 from urllib.request import urlopen
 
+# gets latest dweet for a thing
 def get(thing, b):
-	if b==True:
+	if b==True: # prints dweet info to console
 		print("Fetching latest dweet for '" + thing + "' ...")
 		url = "https://dweet.io/get/latest/dweet/for/" + thing
 
@@ -25,8 +26,9 @@ def get(thing, b):
 				   print("content = " + key)
 
 			print("time = " + data["with"][0]["created"])
+			
 
-	elif b==False:
+	elif b==False: # returns data in list with format: [name of thing, latest dweet, time of dweet]
 		l = [thing, False, False]
 
 		url = "https://dweet.io/get/latest/dweet/for/" + thing
@@ -36,7 +38,7 @@ def get(thing, b):
 		uClient.close()
 		data = json.loads(data)
 
-		if "because" in data:
+		if "because" in data: # if unable to fetch latest dweet, second 2 elements in list will be False
 			return l
 
 		else:
@@ -49,9 +51,10 @@ def get(thing, b):
 
 			return l
 
-	else:
-		print("b must be a bool; True prints basic data, False returns data in a list")
+	else: # b was not a bool
+		print("b must be a bool; True prints data to console, False returns data in a list")
 
+# sends dweet for thing with content
 def send(thing, content):
 	url = "https://dweet.io/dweet/for/" + thing + "?" + content
 
@@ -62,9 +65,9 @@ def send(thing, content):
 
 	l = [False, False, False, False]
 
-	if "because" in data:
+	if "because" in data: # if unable to send dweet, all elements in list will be False
 		return l
 
-	else:
+	else: # returns list with format: [name of thing, content dweeted, time of dweet, dweet transaction code
 		l = [thing, content, data["with"]["created"], data["with"]["transaction"]]
 		return l
